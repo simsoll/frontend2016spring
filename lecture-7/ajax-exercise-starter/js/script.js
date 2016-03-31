6196bb81638210851911d2b3ece787842ed4c1db
Simon Søllingvraa <sso@danskecommodities.com>
2016-03-31 23:56:06 +0200

    Added solutions to AJAX exercise 1 and AJAX exercise 2

--- lecture-7/ajax-exercise-starter/js/script.js
+++ lecture-7/ajax-exercise-starter/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 (function($){
 	"use strict";
 
@@ -21,4 +22,88 @@
   }
 
 })(jQuery);
+=======
+(function() {
+    "use strict";
 
+    var module = function() {
+        function initForm() {
+            $('#submit').on('click', function(e) {
+                e.preventDefault();
+                $.ajax({
+                    type: "POST",
+                    dataType: "JSON",
+                    url: "",
+                    data: $('form').serializeArray()
+                })
+                    .done(function(msg) {
+                        alert(JSON.stringify(msg));
+                    });
+            });
+        }
+
+        function initUsers() {
+            $.ajax({
+                url: "https://api.github.com/users"
+            }).success(function(users) {
+                var table = $('#logins');
+
+                $(users).each(function(index, element) {
+                    var $img = $('<img>').attr('src', element.avatar_url);
+                    var $button = $('<button/>',
+                        {
+                            text: 'Load ' + element.login + '\'s repo!',
+                            click: function() {
+                                updateRepos(element.repos_url);
+                            }
+                        });
+
+                    table.append(
+                        $('<tr>').append($('<td>').html(element.login), $('<td>').append($img), $('<td>').append($button))
+                    );
+                });
+            });
+        }
+
+        function initRepos() {
+            $('#repos').change(writeChangeToConsole);
+        }
+
+>>>>>>> Added solutions to AJAX exercise 1 and AJAX exercise 2
+
+        function updateRepos(url) {
+            $.ajax({
+                url: url
+            }).success(function(data) {
+                var selection = $('#repos').empty();
+
+                $(data).each(function(index, element) {
+                    selection.append(
+                        $('<option>').attr('value', element.name).html(element.name)
+                    );
+                });
+            });
+        }
+
+        function writeChangeToConsole() {
+            var selectBox = document.getElementById("repos");
+
+            if (selectBox.options.length === 0) {
+                return;
+            }
+
+            var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+            console.log(selectedValue);
+        }
+        
+        return {
+            init: function() {
+                initForm();
+                initUsers();
+                initRepos();
+            }
+        }
+    } ();
+    
+    module.init();
+})();
--- lecture-7/ajax-exercise-starter/server.js
+++ lecture-7/ajax-exercise-starter/server.js
@@ -11,5 +11,9 @@ app.post('/', function(req, res, next) {
   res.json(req.body);
 });
 
+<<<<<<< HEAD
 app.listen(3000);
 
+=======
+app.listen(4000);
+>>>>>>> Added solutions to AJAX exercise 1 and AJAX exercise 2
