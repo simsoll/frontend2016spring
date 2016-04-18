ef643d7345847ef977a8eae3c2af3e5c62536a82
Niels Henrik Juul Hansen <niha@eaaa.dk>
2016-04-18 15:04:33 +0200

    added lecture-10/sass-starter

---
+++ lecture-10/sass-starter/gulpconfig.js
@@ -0,0 +1,18 @@
+function gulpConfig(){
+    "use strict";
+	
+    var root = "./";
+  
+	var config = {
+		root: root,
+		browserSyncFiles: [root + "**/*"],
+		browserSyncPort: 4000,
+        browsers: ["google chrome", "firefox"],
+        sass: root + "sass/style.scss",
+        css: root + "./css"
+	};
+
+	return config;
+}
+
+module.exports = gulpConfig;
